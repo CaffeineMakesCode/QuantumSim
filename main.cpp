26167126193ad037b6707e5a21988792f7b2d38a
Ravi Budhrani <rbudhrani@connect.ust.hk>
2020-04-30 18:40:36 +0200

    testing Grover's algorithm: -2 qubits -solution |00>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <complex>
+#include "definitions.h"
+#include "QubitLayer.h"
 
 int main(){
-    std::cout<<"V2 non-unitary begins!\n";
+    qubitLayer qubits[numStates*2];
+    qubits[0] = {1,0};
+    QubitLayer q{qubits};
+    for (int i = 0; i < numQubits; i++)
+        q.hadamard(i);
+    for (int i = 0; i < numQubits; i++)
+        q.pauliX(i);
+    q.cphase(0, 1);
+    for (int i = 0; i < numQubits; i++)
+        q.pauliX(i);
+    for (int i = 0; i < numQubits; i++)
+        q.hadamard(i);
+    for (int i = 0; i < numQubits; i++)
+        q.pauliX(i);
+    q.cphase(0, 1);
+    for (int i = 0; i < numQubits; i++)
+        q.pauliX(i);
+    for (int i = 0; i < numQubits; i++)
+        q.hadamard(i);
+    q.printQubits();
 }
