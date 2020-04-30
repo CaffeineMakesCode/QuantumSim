5c607faf990f4ef3b8280a5e07c40a70cfe1c735
Ravi Budhrani <rbudhrani@connect.ust.hk>
2020-04-30 18:19:50 +0200

    added constants and typdefs

---
+++ definitions.h
@@ -0,0 +1,17 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+#include <complex>
+
+constexpr int numQubits{2};
+constexpr std::complex<double> hadamardCoef{0.707106781186548,0};
+constexpr std::complex<double> complexImg{0,1};
+constexpr int power2(int qubits){
+    int result{1};
+    for (int i = 0; i < qubits; i++)
+        result *= 2;
+    return result;
+}
+constexpr int numStates{power2(numQubits)};
+typedef std::complex<double> qubitLayer;
+
+#endif
