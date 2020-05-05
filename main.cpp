b095427d47fef5b37fde17421682682705207ac2
Ravi Budhrani <rbudhrani@connect.ust.hk>
2020-05-05 21:54:50 +0200

    matching output with multithreading branch

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include "qAlgorithms.h"
 
 int main(){
-    QubitLayer q = grover(10);
-    q.printMeasurement();
-    q.printQubits();
+    auto start = std::chrono::system_clock::now();
+    QubitLayer q = grover(3);
+    std::chrono::duration<double> dur= std::chrono::system_clock::now() - start;
+    std::cout << "Time for program " << dur.count() << " seconds" << std::endl;
 }
