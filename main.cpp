0d98f23ed0ff4eed915b9554f6e248213802667e
Ravi Budhrani <rbudhrani@connect.ust.hk>
2020-05-15 19:54:44 +0200

    solution for Grover will always be 0 for testing

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main(){
     //auto start = std::chrono::system_clock::now();
     //pauliError error = errorX;
-    QubitLayer q = grover(10);
+    QubitLayer q = grover(0);
     //std::chrono::duration<double> dur= std::chrono::system_clock::now() - start;
     //std::cout << "Time for program " << dur.count() << " seconds" << std::endl;
     //q.printMeasurement();
