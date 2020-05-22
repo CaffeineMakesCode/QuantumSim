dcdcaff3aec096346164aa052cf919cfcaae7dee
Ravi Budhrani <rbudhrani@connect.ust.hk>
2020-05-22 17:57:06 +0200

    epr pair generation benchmark

---
+++ benchmarks/epr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <complex>
+#include <string>
+#include "timers.hpp"
+#include "../QubitLayer.hpp"
+#include "../definitions.hpp"
+
+int main(){
+    if (numQubits < 2){
+        std::cout<<"\033[31;31m[Error]\033[m"<<std::endl;
+        std::cout<<"Number of qubits defined:           "<<numQubits<<std::endl;
+        std::cout<<"Minimum number of qubits needed:    "<<2<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+    QubitLayer q;
+    start = Clock::now();
+    //apply hadamard to the first qubit
+    q.hadamard(numQubits-1);
+    //apply cnot on the 2 qubits
+    q.cnot(numQubits-1, 0);
+    stop = Clock::now();
+    duration = std::chrono::duration_cast<mus>(stop-start).count();
+    std::cout << "Execution time ("<< numQubits <<" qubits): " << duration << " µs" << std::endl;
+}
