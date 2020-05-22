9c9e2ddccd16dae11907b23681e1a9b814276dc9
Ravi Budhrani <rbudhrani@connect.ust.hk>
2020-05-22 17:54:09 +0200

    added microseconds to timer

--- benchmarks/timers.hpp
+++ benchmarks/timers.hpp
@@ -4,6 +4,7 @@
 
 typedef std::chrono::steady_clock Clock;
 typedef std::chrono::nanoseconds ns;
+typedef std::chrono::microseconds mus;
 typedef std::chrono::duration<double> dsec;
 typedef Clock::time_point timePoint;
 
