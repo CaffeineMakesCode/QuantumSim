b247f463d8f4215dfdcddfbe537f25479a17b885
Ravi Budhrani <rbudhrani@connect.ust.hk>
2020-05-25 03:25:05 +0200

    changed clock to steady_clock

--- main.cpp
+++ main.cpp
@@ -4,14 +4,10 @@
 #include "qAlgorithms.hpp"
 
 int main(){
-    auto start = std::chrono::high_resolution_clock::now();
-    //pauliError error = errorX;
+    auto start = std::chrono::steady_clock::now();
     QubitLayer q = grover(0);
-    //QubitLayer q;
-    //q.pauliX(0);
-    auto stop = std::chrono::high_resolution_clock::now();
+    auto stop = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
     std::cout << "Execution time: " << duration << " µs" << std::endl;
     q.printMeasurement();
-    //q.printQubits();
 }
