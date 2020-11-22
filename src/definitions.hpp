b7c96f384b569682d2e88e3dcb8d52a935e395b1
rbudhrani <rbudhrani@connect.ust.hk>
2020-11-22 16:39:40 +0100

    changed numQubits and numStates types

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -10,13 +10,13 @@ constexpr precision pi{3.14159265358979323846};
 constexpr std::complex<precision> hadamardCoef{0.707106781186548,0};
 constexpr std::complex<precision> complexImg{0,1};
 constexpr std::complex<precision> zeroComplex{0,0};
-constexpr int power2(int qubits){
+constexpr unsigned long long int power2(unsigned int qubits){
     int result{1};
     for (int i = 0; i < qubits; i++)
         result *= 2;
     return result;
 }
-constexpr int numStates{power2(numQubits)};
+constexpr unsigned long long int numStates{power2(numQubits)};
 typedef std::complex<precision> qubitLayer;
 
 #endif
