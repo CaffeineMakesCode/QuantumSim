f2aa392c5964c82f8d8699910413e9ccd2f260a2
rbudhrani <rbudhrani@connect.ust.hk>
2020-11-23 22:29:49 +0100

    updated examples

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 int main(){
     auto start = std::chrono::steady_clock::now();
-    QubitLayer q = grover(0);
+    QubitLayer q = grover(2, 0);
     auto stop = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
     std::cout << "Execution time: " << duration << " µs" << std::endl;
--- src/qAlgorithms.hpp
+++ src/qAlgorithms.hpp
@@ -2,32 +2,32 @@
 #define QALGORITHMS_H
 #include "QubitLayer.hpp"
 
-enum pauliError {errorX, errorY, errorZ};
+enum pauliError { errorX, errorY, errorZ };
 
 /**
  * Performs Grover search on 'n' qubits.
  * 'n' is defined as numQubits in defitions.h
- * 
+ * @param numQubits number of qubits
  * @param dSolution location of the solution in decimal
  * @param numReps   optional parameter to specify the number of Grover iterations
  * @return QubitLayer object containing the amplitudes of all the state
  */
-QubitLayer grover(int dSolution, int numReps = 0);
+QubitLayer grover(unsigned int numQubits, int unsigned dSolution, int unsigned numReps = 0);
 /**
  * 3 qubit repetition code.
- * 
+ * @param numQubits       number of qubits
  * @param errorLocation   qubit where error occured
  * @param errorType       type of error
  * @return QubitLayer object containing the amplitudes of all the state
  */
-QubitLayer repCode3(int errorLocation, pauliError errorType);
+QubitLayer repCode3(unsigned int numQubits, unsigned int errorLocation, pauliError errorType);
 /**
  * Generates an EPR pair between 2 qubits.
- * 
- * @param q1   first qubit in pair
- * @param q2   second qubit in pair
+ * @param numQubits number of qubits
+ * @param q1        first qubit in pair
+ * @param q2        second qubit in pair
  * @return QubitLayer object containing the amplitudes of all the state
  */
-QubitLayer genEPR(int q1, int q2);
+QubitLayer genEPR(unsigned int numQubits, unsigned int q1, unsigned int q2);
 
 #endif
