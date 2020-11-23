0f36bb6acc435c486485ba4fa98b80cdb8f22f9d
rbudhrani <rbudhrani@connect.ust.hk>
2020-11-23 17:23:28 +0100

    removed numQubits and numStates from definitions

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -1,6 +1,3 @@
-#ifndef numQubits
-#define numQubits 2
-#endif
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 #include <complex>
@@ -10,14 +7,7 @@ constexpr precision pi{3.14159265358979323846};
 constexpr std::complex<precision> hadamardCoef{0.707106781186548,0};
 constexpr std::complex<precision> complexImg{0,1};
 constexpr std::complex<precision> zeroComplex{0,0};
-constexpr unsigned long long int power2(unsigned int qubits){
-    unsigned long long int result{1};
-    for (unsigned int i = 0; i < qubits; i++)
-        result *= 2;
-    return result;
-}
 constexpr unsigned int maxQubits{2^32-1};                               //max qubits allowed by QuantumSim is 2^32
-constexpr unsigned long long int numStates{power2(numQubits)};
 typedef std::complex<precision> qubitLayer;
 
 #endif
