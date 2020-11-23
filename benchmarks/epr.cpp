06315523dbc96bd674b18b7c2e064d3b9d52b8cd
rbudhrani <rbudhrani@connect.ust.hk>
2020-11-23 17:33:21 +0100

    fixed epr benchmarking

--- benchmarks/epr.cpp
+++ benchmarks/epr.cpp
@@ -6,13 +6,8 @@
 #include "../src/definitions.hpp"
 
 int main(){
-    if (numQubits < 2){
-        std::cout<<"\033[31;31m[Error]\033[m"<<std::endl;
-        std::cout<<"Number of qubits defined:           "<<numQubits<<std::endl;
-        std::cout<<"Minimum number of qubits needed:    "<<2<<std::endl;
-        exit(EXIT_FAILURE);
-    }
-    QubitLayer q;
+    unsigned int numQubits = 2;
+    QubitLayer q(numQubits);
     start = Clock::now();
     //apply hadamard to the first qubit
     q.hadamard(1);
@@ -21,4 +16,5 @@ int main(){
     stop = Clock::now();
     duration = std::chrono::duration_cast<mus>(stop-start).count();
     std::cout << "Execution time ("<< numQubits <<" qubits): " << duration << " µs" << std::endl;
+    q.printQubits();
 }
