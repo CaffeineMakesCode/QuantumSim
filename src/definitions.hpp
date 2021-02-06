a3279a7e93d4aa6b418689dd451d7ab8559f0c5e
rbudhrani <rbudhrani@connect.ust.hk>
2021-02-06 22:54:05 +0100

    added CLI flag for parallel impl

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -4,10 +4,11 @@
 
 typedef double precision;
 constexpr precision pi{3.14159265358979323846};
-constexpr std::complex<precision> hadamardCoef{0.707106781186548,0};
-constexpr std::complex<precision> complexImg{0,1};
-constexpr std::complex<precision> zeroComplex{0,0};
-constexpr unsigned int maxQubits{2^32-1};                               //max qubits allowed by QuantumSim is 2^32
+constexpr std::complex<precision> hadamardCoef{0.707106781186548, 0};
+constexpr std::complex<precision> complexImg{0, 1};
+constexpr std::complex<precision> zeroComplex{0, 0};
+constexpr unsigned int maxQubits{2 ^ 32 - 1}; //max qubits allowed by QuantumSim is 2^32
 typedef std::complex<precision> qubitLayer;
+static bool isParallel{false};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,14 @@
 #include "QubitLayer.hpp"
 #include "../examples/qAlgorithms.hpp"
 
-int main(){
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < argc; i++)
+        if (std::string(argv[i]) == "-p")
+        {
+            isParallel = true;
+            std::cout << "Running parallel version of QuantumSim" << std::endl;
+        }
     auto start = std::chrono::steady_clock::now();
     QubitLayer q = grover(2, 0);
     auto stop = std::chrono::steady_clock::now();
