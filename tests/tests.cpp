88eaa9ab190115b0b11d05ff95aadd2421e99753
rbudhrani <rbudhrani@connect.ust.hk>
2021-02-07 17:30:53 +0100

    testing parallel and sequential versions

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,29 +4,83 @@
 #include "tests.hpp"
 
 // list of quantum gates
-enum Gates { X, Y, Z, rx, ry, rz, cnot, cphase, toffoli, mcnot, Hadamard, mcphase };
+enum Gates
+{
+    X,
+    Y,
+    Z,
+    rx,
+    ry,
+    rz,
+    cnot,
+    cphase,
+    toffoli,
+    mcnot,
+    Hadamard,
+    mcphase
+};
 
-bool testGate(Gates gate){
+bool testGate(Gates gate)
+{
     // initalise state to |111> so all gates can change the state
-    qubitLayer testInput[8] = {zeroComplex, zeroComplex, zeroComplex, zeroComplex, zeroComplex, zeroComplex, zeroComplex, {1,0}};
+    qubitLayer testInput[8] = {zeroComplex, zeroComplex, zeroComplex, zeroComplex, zeroComplex, zeroComplex, zeroComplex, {1, 0}};
     QubitLayer q = QubitLayer(3, testInput);
     // define array of control qubits for multiple control qubit gates
-    int ctrlQubits[2]{0,1};
+    int ctrlQubits[2]{0, 1};
     TesterGate testerGate;
-    switch(gate){
-        case X : q.pauliX(0); testerGate = pauliXtester; break;
-        case Y : q.pauliY(0); testerGate = pauliYtester; break;
-        case Z : q.pauliZ(0); testerGate = pauliZtester; break;
-        case Hadamard : q.hadamard(0); testerGate = hadamardTester; break;
-        case rx : q.rx(0, pi); testerGate = rxTester; break;
-        case ry : q.ry(0, pi); testerGate = ryTester; break;
-        case rz : q.rz(0, pi); testerGate = rzTester; break;
-        case cnot : q.cnot(0, 1); testerGate = cnotTester; break;
-        case cphase : q.cphase(0, 1); testerGate = cphaseTester; break;
-        case toffoli : q.toffoli(0, 1, 2); testerGate = toffoliTester; break;
-        case mcnot : q.mcnot(ctrlQubits, 2, 2); testerGate = mcnotTester; break;
-        case mcphase : q.mcphase(ctrlQubits, 2, 2); testerGate = mcphaseTester; break;
-        default : q.pauliX(0); break;
+    switch (gate)
+    {
+    case X:
+        q.pauliX(0);
+        testerGate = pauliXtester;
+        break;
+    case Y:
+        q.pauliY(0);
+        testerGate = pauliYtester;
+        break;
+    case Z:
+        q.pauliZ(0);
+        testerGate = pauliZtester;
+        break;
+    case Hadamard:
+        q.hadamard(0);
+        testerGate = hadamardTester;
+        break;
+    case rx:
+        q.rx(0, pi);
+        testerGate = rxTester;
+        break;
+    case ry:
+        q.ry(0, pi);
+        testerGate = ryTester;
+        break;
+    case rz:
+        q.rz(0, pi);
+        testerGate = rzTester;
+        break;
+    case cnot:
+        q.cnot(0, 1);
+        testerGate = cnotTester;
+        break;
+    case cphase:
+        q.cphase(0, 1);
+        testerGate = cphaseTester;
+        break;
+    case toffoli:
+        q.toffoli(0, 1, 2);
+        testerGate = toffoliTester;
+        break;
+    case mcnot:
+        q.mcnot(ctrlQubits, 2, 2);
+        testerGate = mcnotTester;
+        break;
+    case mcphase:
+        q.mcphase(ctrlQubits, 2, 2);
+        testerGate = mcphaseTester;
+        break;
+    default:
+        q.pauliX(0);
+        break;
     }
     // define variable to store the result of the test
     bool testResult = true;
@@ -37,7 +91,16 @@ bool testGate(Gates gate){
     return testResult;
 }
 
-int main(){
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < argc; i++)
+        if (std::string(argv[i]) == "-p")
+        {
+            isParallel = true;
+            std::cout << "Testing parallel version of QuantumSim" << std::endl;
+        }
+        else
+            std::cout << "Testing sequential version of QuantumSim" << std::endl;
     // define variable to store result of the tests
     bool testResult = true;
     std::cout << "\033[34;34m===========Test Results===========\033[m" << std::endl;
