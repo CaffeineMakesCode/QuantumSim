9a3cd417719c188ac243b0e8ede92eef2e572f3a
rbudhrani <rbudhrani@connect.ust.hk>
2021-02-07 18:58:26 +0100

    fix max qubits const

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -7,7 +7,7 @@ constexpr precision pi{3.14159265358979323846};
 constexpr std::complex<precision> hadamardCoef{0.707106781186548, 0};
 constexpr std::complex<precision> complexImg{0, 1};
 constexpr std::complex<precision> zeroComplex{0, 0};
-constexpr unsigned int maxQubits{(2 ^ 32) - 1}; //max qubits allowed by QuantumSim is 2^32
+constexpr unsigned int maxQubits{32}; //max states allowed by QuantumSim is 2^32
 typedef std::complex<precision> qubitLayer;
 static bool isParallel{false};
 
