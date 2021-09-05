7badd9a16bbace1d48f0c44a405c3fcd5fc767b5
rbudhrani <rbudhrani@connect.ust.hk>
2021-09-05 22:40:43 +0200

    qasm parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <complex>
-#include <chrono>
+#include <antlr4-common.h>
+#include "qasm3Parser.h"
+#include "qasm3Lexer.h"
+#include "Qasm3CustomListener.hpp"
 #include "QubitLayer.hpp"
-#include "../examples/qAlgorithms.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -12,10 +15,26 @@ int main(int argc, char *argv[])
             isParallel = true;
             std::cout << "Running parallel version of QuantumSim" << std::endl;
         }
-    auto start = std::chrono::steady_clock::now();
-    QubitLayer q = grover(2, 0);
-    auto stop = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
-    std::cout << "Execution time: " << duration << " µs" << std::endl;
-    q.printMeasurement();
+    std::ifstream infile;
+    if (argc > 1)
+    {
+        infile.open(argv[1], std::ios::in);
+        // Provide the input text in a stream
+        antlr4::ANTLRInputStream input(infile);
+
+        // Create a lexer which scans the input stream
+        // to create a token stream.
+        qasm3Lexer lexer(&input);
+        antlr4::CommonTokenStream tokens(&lexer);
+
+        // Create a parser which parses the token stream
+        // to create a parse tree.
+        qasm3Parser parser(&tokens);
+        antlr4::tree::ParseTree *tree = parser.program();
+
+        Qasm3CustomListener listener;
+
+        antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+        infile.close();
+    }
 }
