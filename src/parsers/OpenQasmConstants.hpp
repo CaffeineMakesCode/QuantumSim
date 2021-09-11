5795610cfd3ddb624dd76609d1916214ccfb836c
rbudhrani <rbudhrani@connect.ust.hk>
2021-09-11 13:46:06 +0200

    added qasm constants

---
+++ src/parsers/OpenQasmConstants.hpp
@@ -0,0 +1,8 @@
+#ifndef OPENQASM_CONSTANTS_H
+#define OPENQASM_CONSTANTS_H
+#include <string_view>
+
+constexpr std::string_view OPENQASM_HADAMARD_GATE{"h"};
+constexpr std::string_view OPENQASM_CNOT_GATE{"cx"};
+
+#endif
