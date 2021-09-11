ea086cd078ed19c6e6bfe71f7e852fd6baad1749
rbudhrani <rbudhrani@connect.ust.hk>
2021-09-11 13:45:53 +0200

    extended custom listener

--- src/parsers/Qasm3CustomListener.cpp
+++ src/parsers/Qasm3CustomListener.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
+#include "OpenQasmConstants.hpp"
 #include "Qasm3CustomListener.hpp"
-#include "QubitLayer.hpp"
 
 void Qasm3CustomListener::enterHeader(qasm3Parser::HeaderContext *ctx)
 {
     std::cout << ctx->toStringTree() << std::endl;
 }
 
+void Qasm3CustomListener::exitProgram(qasm3Parser::ProgramContext *ctx)
+{
+    //print out qubits at the end of the program
+    layer->printQubits();
+}
+
 void Qasm3CustomListener::enterQuantumDeclarationStatement(qasm3Parser::QuantumDeclarationStatementContext *ctx)
 {
+    //get the number of qubits and create a QubitLayer object
     antlr4::tree::TerminalNode *numQubits = ctx->quantumDeclaration()->designator()->expression()->expressionTerminator()->Integer();
-    std::cout << numQubits->toString() << std::endl;
+    unsigned int n = std::stoi(numQubits->toString());
+    layer = new QubitLayer(n);
 }
 
 void Qasm3CustomListener::enterQuantumInstruction(qasm3Parser::QuantumInstructionContext *ctx)
 {
+    //get the gate and the index of the qubit(s)
     std::string gate = ctx->quantumGateCall()->quantumGateName()->Identifier()->toString();
-    std::vector<qasm3Parser::IndexIdentifierContext *> qubitIndices = ctx->quantumGateCall()->indexIdentifierList()->indexIdentifier();
-    for (qasm3Parser::IndexIdentifierContext *q : qubitIndices)
+    std::vector<qasm3Parser::IndexIdentifierContext *> qubits = ctx->quantumGateCall()->indexIdentifierList()->indexIdentifier();
+    applyGate(gate, qubits);
+}
+
+void Qasm3CustomListener::applyGate(std::string gate, std::vector<qasm3Parser::IndexIdentifierContext *> qubits)
+{
+    //check for available gates defined in QubitLayer
+    if (gate == OPENQASM_HADAMARD_GATE)
     {
-        std::vector<qasm3Parser::ExpressionContext *> indices = q->expressionList()->expression();
-        for (qasm3Parser::ExpressionContext *i : indices)
-            std::cout << i->expressionTerminator()->Integer()->toString() << std::endl;
+        int i = std::stoi(qubits[0]->expressionList()->expression(0)->expressionTerminator()->Integer()->toString());
+        layer->hadamard(i);
+    }
+    if (gate == OPENQASM_CNOT_GATE)
+    {
+        layer->cnot(std::stoi(qubits[0]->expressionList()->expression(0)->expressionTerminator()->Integer()->toString()),
+                    std::stoi(qubits[1]->expressionList()->expression(0)->expressionTerminator()->Integer()->toString()));
     }
 }
--- src/parsers/Qasm3CustomListener.hpp
+++ src/parsers/Qasm3CustomListener.hpp
@@ -2,6 +2,7 @@
 #define QASM3CUSTOMLISTENER_H
 #include "qasm3BaseListener.h"
 #include "qasm3Parser.h"
+#include "QubitLayer.hpp"
 
 class Qasm3CustomListener : public qasm3BaseListener
 {
@@ -9,8 +10,11 @@ public:
     void enterHeader(qasm3Parser::HeaderContext *);
     void enterQuantumDeclarationStatement(qasm3Parser::QuantumDeclarationStatementContext *);
     void enterQuantumInstruction(qasm3Parser::QuantumInstructionContext *);
+    void exitProgram(qasm3Parser::ProgramContext *);
+    void applyGate(std::string gate, std::vector<qasm3Parser::IndexIdentifierContext *> qubits);
 
 private:
+    QubitLayer *layer = nullptr;
 };
 
 #endif
