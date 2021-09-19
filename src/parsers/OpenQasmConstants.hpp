1122575669981de3c055b67240dc242dd6ecdb16
rbudhrani <rbudhrani@connect.ust.hk>
2021-09-19 21:06:06 +0200

    added standard openqasm gates

--- src/parsers/OpenQasmConstants.hpp
+++ src/parsers/OpenQasmConstants.hpp
@@ -2,7 +2,15 @@
 #define OPENQASM_CONSTANTS_H
 #include <string_view>
 
+constexpr std::string_view OPENQASM_X_GATE{"x"};
+constexpr std::string_view OPENQASM_Y_GATE{"y"};
+constexpr std::string_view OPENQASM_Z_GATE{"z"};
 constexpr std::string_view OPENQASM_HADAMARD_GATE{"h"};
+constexpr std::string_view OPENQASM_RX_GATE{"rx"};
+constexpr std::string_view OPENQASM_RY_GATE{"ry"};
+constexpr std::string_view OPENQASM_RZ_GATE{"rz"};
 constexpr std::string_view OPENQASM_CNOT_GATE{"cx"};
+constexpr std::string_view OPENQASM_TOFFOLI_GATE{"ccx"};
+constexpr std::string_view OPENQASM_CPHASE_GATE{"cphase"};
 
 #endif
